9a3c50ff7412f2c1de51a79c4b6128e568eaa14e
daviddicken <davidfromseattle@gmail.com>
2020-06-13 17:39:45 -0700

    touch up work

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var allStores = [];
-var storeHours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm","4pm", "5pm", "6pm", "7pm"];
-var seattle = new Store("Seattle", 23, 65, 6.5);
-var dubai = new Store("Dubai", 11, 38, 3.7);
-var paris = new Store("Paris", 20, 38, 2.3);
-var lima = new Store("Lima", 2, 16, 4.6);
-var tokyo = new Store("Tokyo", 3, 24, 1.2);
+var allStores = [];                                   
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm','4pm', '5pm', '6pm', '7pm'];                                                
+var seattle = new Store('Seattle', 23, 65, 6.5);
+var dubai = new Store('Dubai', 11, 38, 3.7);
+var paris = new Store('Paris', 20, 38, 2.3);
+var lima = new Store('Lima', 2, 16, 4.6);
+var tokyo = new Store('Tokyo', 3, 24, 1.2);
 
 Store.prototype.tableData = tableData;
 //Store.prototype.newStore = newStoreForm;
@@ -25,8 +25,8 @@ var maxCustInput = theForm.maxCustomer;
 var avgCookieInput = theForm.avgCookie;
 
 var storeValue = storeInput.value;
-var minCustValue = minCustInput.value;// parseInt(minCustInput.value);
-var maxCustValue = maxCustInput.value;//parseInt(maxCustInput.value);
+var minCustValue = minCustInput.value;
+var maxCustValue = maxCustInput.value;
 var avgCookieValue = parseFloat(avgCookieInput.value);
 
 var formStore = new Store(storeValue, minCustValue, maxCustValue, avgCookieValue);
@@ -64,22 +64,39 @@ function rebuildTable()                              //function to clear and reb
   createTable();                                     // create new table
 }
 //-------------------------------------------------------------------------
+
+// non working function==================================================
+function createCell(targetId, rowType,cellType, theInput)
+ {
+  //var targetId = document.getElementById(elId);              // find table
+  var createRow = document.createElement(rowType);        // create row
+  var createCell = document.createElement(cellType);      // create cell
+  createCell.textContent = theInput;
+  createRow.appendChild(createCell);
+  targetId.appendChild(createRow);
+
+ }
+
+
 function tableHeader() //function for creating header with hours
 {
-  var table = document.getElementById("storeTable");  // find table
-  var headerRow = document.createElement("tr");       // create row
-  var headerCell = document.createElement("th");      // create cell
-  headerCell.textContent = "Store";                   // add "Store" text to cell
+  
+  var table = document.getElementById('storeTable');  // find table
+  //createCell(table,'tr','th','store');
+  var headerRow = document.createElement('tr');       // create row
+  var headerCell = document.createElement('th');      // create cell
+
+  headerCell.textContent = 'Store';                   // add 'Store' text to cell
   headerRow.appendChild(headerCell);                  // attach cell to row
   
   for( var i = 0; i < storeHours.length; i++)          // loop through storeHours array to fill cells w/hours
   {  
-    var nextCell = document.createElement("th");      // create cell
+    var nextCell = document.createElement('th');      // create cell
     nextCell.textContent = storeHours[i];             // fill cell with value in i index of storeHours array
     headerRow.appendChild(nextCell);                  // attach cell to row
   }
-    var totalCell = document.createElement("th");     // create cell
-    totalCell.textContent = "Totals";                 // put text "Totals" in last cell on header
+    var totalCell = document.createElement('th');     // create cell
+    totalCell.textContent = 'Totals';                 // put text 'Totals' in last cell on header
     headerRow.appendChild(totalCell);                 // attatch cell to row
     table.appendChild(headerRow);                     // attach row to table
 }
@@ -88,15 +105,15 @@ function tableData()                         //function for filling table with c
 {
   var cookieTotal = 0;                                        // keeps track of cookies sold all day
   var tempCookie = 0;                                         // holds random number generated
-  var table = document.getElementById("storeTable");          // find table
-  var dataRow = document.createElement("tr");                 // create row
-  var dataCell = document.createElement("td");                // create cell
+  var table = document.getElementById('storeTable');          // find table
+  var dataRow = document.createElement('tr');                 // create row
+  var dataCell = document.createElement('td');                // create cell
   dataCell.textContent = this.store;                          // put store name in cell
   dataRow.appendChild(dataCell);                              // attach to row
 
   for(var i = 0; i < storeHours.length; i++)                  // for loop to fill each hour with sales        
   {
-    var nextCell = document.createElement("td");              // create cell
+    var nextCell = document.createElement('td');              // create cell
     tempCookie = Math.ceil(randomCustomer(this.minCust, this.maxCust) * this.avgCookie); //random number
     nextCell.textContent = tempCookie;                        // puts random number in cell
     this.dailyTotals.push(tempCookie);                        // pushes random number to dailytotals array
@@ -104,7 +121,7 @@ function tableData()                         //function for filling table with c
     cookieTotal += tempCookie;                                // adds random number to cookie total
   }
 
-  var totalCell = document.createElement("th");               // create cell for day total
+  var totalCell = document.createElement('th');               // create cell for day total
   totalCell.textContent = cookieTotal;                        // put cookie total in cell
   dataRow.appendChild(totalCell);                             // attach cell to row
   table.appendChild(dataRow);                                 // attach row to table
@@ -112,15 +129,15 @@ function tableData()                         //function for filling table with c
 //-----------------------------------------------------------------------------------------
 function hourlyTotal() //footer function to get hourly totals and creating table footer
 {
-  var table = document.getElementById("storeTable");                 // find table
-  var lastRow = document.createElement("tr");                        // create row
-  var totalCell = document.createElement("th");                             // create cell
-  totalCell.textContent = "Totals:";                                 // put text in cell
+  var table = document.getElementById('storeTable');                 // find table
+  var lastRow = document.createElement('tr');                        // create row
+  var totalCell = document.createElement('th');                             // create cell
+  totalCell.textContent = 'Totals:';                                 // put text in cell
   lastRow.appendChild(totalCell);                                    // attach cell to row
   
   for (var i = 0; i < storeHours.length; i++) //get total cookies from each store and each hour 
   {
-    var totalCell = document.createElement("th");                   // create cell
+    var totalCell = document.createElement('th');                   // create cell
     
     var hourTotal = 0;                                              // var to hold and add each stores index value
     for (var j = 0; j < allStores.length; j++)                      // get index value for each hour from each store
@@ -132,7 +149,7 @@ function hourlyTotal() //footer function to get hourly totals and creating table
     lastRow.appendChild(totalCell);                                // attach cell
   }
   //**************** This cell doesn't get cleared by rebuild function?? **************************************** 
-  var totalCell = document.createElement("th");                    // create cell for total of all stores today
+  var totalCell = document.createElement('th');                    // create cell for total of all stores today
   var dayTotal = 0;                                                // var to hold total for all stores
   for (var k = 0; k < allStores.length; k++)                       // to loop through allstores array
   {
